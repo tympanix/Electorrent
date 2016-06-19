496f866612d58894e8c2f6a38d481cbcbf77e600
Mathias Mortensen <tympanix@gmail.com>
2016-06-19 17:38:02 +0200

    Progress bar and sorting
    Performance issues needs to be resolved

---
+++ scripts/filters/toArray.js
@@ -0,0 +1,9 @@
+angular.module("torrentApp").filter("toArray", function(){
+    return function(obj) {
+        var result = [];
+        angular.forEach(obj, function(val, key) {
+            result.push(val);
+        });
+        return result;
+    };
+});
