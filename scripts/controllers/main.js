515accbcaba93248b29eb8615714bb709aaebd52
Mathias <tympanix@gmail.com>
2016-06-20 02:08:03 +0200

    Torrent filtering by status / UI progress bar

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,14 +1,24 @@
-angular.module("torrentApp").controller("mainController", ["$scope", "$interval", "$log", "utorrentService", function ($scope, $interval, $log, $utorrentService) {
+angular.module("torrentApp").controller("mainController", ["$scope", "$interval", "$filter", "$log", "utorrentService", function ($scope, $interval, $filter, $log, $utorrentService) {
     var ut = $utorrentService;
 
     ut.init();
 
     $scope.torrents = {};
+    $scope.arrayTorrents = [];
+
+    $scope.filters = {
+        status: 'downloading'
+    }
 
     $scope.name = "Mathias";
 
     $scope.download = function(){
-        ut.addTorrentUrl("magnet:?xt=urn:btih:ce8357ded670f06329f6028d2f2cea6f514646e0&dn=Zootopia+2016+1080p+HDRip+x264+AC3-JYK&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969");
+        ut.addTorrentUrl("magnet:?xt=urn:btih:B8E6C2551CD060F1D31657C11787DF9F65AE5A13&dn=orange+is+the+new+black+s04e01+webrip+xvid+mp3+rarbg&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce");
+    }
+
+    $scope.filterByStatus = function(status){
+        $scope.filters.status = status;
+        refreshTorrents();
     }
 
     $scope.update = function() {
@@ -19,12 +29,76 @@ angular.module("torrentApp").controller("mainController", ["$scope", "$interval"
         });
     };
 
+    function fetchTorrents(){
+        var results = [];
+        angular.forEach($scope.torrents, function(torrent, hash) {
+            results.push(torrent);
+        });
+        return results;
+    }
+
+    function torrentSorter(){
+        return function(a, b){
+            return b.dateAdded - a.dateAdded;
+        }
+    }
+
+    function torrentFilter(){
+        return function(torrent){
+            var matches = true;
+            if ($scope.filters.status) {
+                switch ($scope.filters.status) {
+                    case 'finished':
+                    {
+                        matches = torrent.isStatusCompleted();
+                        break;
+                    }
+                    case 'downloading':
+                    {
+                        matches = torrent.isStatusDownloading();
+                        break;
+                    }
+                    case 'paused':
+                    {
+                        matches = torrent.isStatusPaused();
+                        break;
+                    }
+                    case 'queued':
+                    {
+                        matches = torrent.isStatusQueued();
+                        break;
+                    }
+                    case 'seeding':
+                    {
+                        matches = torrent.isStatusSeeding();
+                        break;
+                    }
+                    case 'error':
+                    {
+                        matches = torrent.isStatusError();
+                        break;
+                    }
+                }
+            }
+            return matches;
+        }
+    }
+
+    function refreshTorrents(){
+        var torrents = fetchTorrents();
+        torrents = torrents.filter(torrentFilter());
+        torrents = torrents.sort(torrentSorter());
+        $scope.arrayTorrents = torrents;
+        console.log("Torrents", torrents);
+    }
+
     function newTorrents(torrents){
         if (torrents.all && torrents.all.length > 0) {
             for (i = 0; i < torrents.all.length; i++){
                 var torrent = ut.build(torrents.all[i])
                 $scope.torrents[torrent.hash] = torrent;
             }
+            refreshTorrents()
         }
     }
 
@@ -34,6 +108,7 @@ angular.module("torrentApp").controller("mainController", ["$scope", "$interval"
             for (i = 0; i < torrents.deleted.length; i++) {
                 delete $scope.torrents[torrents.deleted[i]];
             }
+            refreshTorrents()
         }
     }
 
@@ -44,11 +119,12 @@ angular.module("torrentApp").controller("mainController", ["$scope", "$interval"
                 var torrent = ut.build(torrents.changed[i]);
                 $scope.torrents[torrent.hash] = torrent;
             }
+            refreshTorrents()
         }
     }
 
     $interval(function(){
         $scope.update();
-    }, 5000)
+    }, 2000)
 
 }])
--- scripts/directives/progress.js
+++ scripts/directives/progress.js
@@ -1,20 +1,24 @@
 angular.module("torrentApp").directive('progress', function() {
     return {
         scope: {
-            percent: '=',
+            progress: '=',
         },
         restrict: 'A',
         link: link
     };
 
     function link(scope, element, attrs) {
-        $(element).progress();
+        var torrent = scope.progress;
+        element.find('.bar').css('width', torrent.percent/10 + '%')
+        element.find('label').html(torrent.statusMessage);
 
-        scope.$watch(function() {return scope.percent; }, function(newValue){
+        scope.$watch(function() {return torrent.percent; }, function(newValue){
             console.log("Percent changed", newValue);
-            $(element).progress({
-                percent: newValue
-            });
+            element.find('.bar').css('width', torrent.percent/10 + '%');
+            element.find('label').html(torrent.statusMessage);
+            if (torrent.isStatusDownloading()){
+                element.find('label').append(' ' + torrent.percent/10 + '%')
+            }
         });
     }
 
