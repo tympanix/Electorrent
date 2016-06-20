49bab5aa3ca16102ac84aa38995f7d1ca4d8b4dc
Mathias <tympanix@gmail.com>
2016-06-20 02:08:03 +0200

    Torrent filtering by status / UI progress bar

---
+++ scripts/filters/bytes.js
@@ -0,0 +1,21 @@
+angular.module("torrentApp").filter('bytes', function() {
+        return function(bytes) {
+            var val;
+            var uom;
+
+            if (bytes < 1024) {
+                val = bytes;
+                uom = 'B';
+            } else if (bytes < 1048576) {
+                val = (bytes / 1024).toFixed(1);
+                uom = 'KB';
+            } else if (bytes < 1073741824) {
+                val = (bytes / 1048576).toFixed(1);
+                uom = 'MB';
+            } else {
+                val = (bytes / 1073741824).toFixed(1);
+                uom = 'GB';
+            }
+            return [val, uom].join(' ');
+        };
+    });
