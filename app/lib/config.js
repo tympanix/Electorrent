229bccb088a3240433d34b7748076b27e62292d5
Mathias <tympanix@gmail.com>
2016-07-10 20:09:02 +0200

    Installers and two package.json layout

---
+++ app/lib/config.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+var data = null;
+
+var defaultSettings = {
+    ui: {
+        resizeMode: 'basic'
+    }
+};
+
+var dataFilePath = null;
+
+function load() {
+    if (data !== null) {
+        return;
+    }
+
+    if (!fs.existsSync(dataFilePath)) {
+        data = copy(defaultSettings);
+        return;
+    }
+
+    data = JSON.parse(fs.readFileSync(dataFilePath, 'utf-8'));
+}
+
+function save(callback) {
+    fs.writeFile(dataFilePath, JSON.stringify(data), callback);
+}
+
+function copy(object) {
+    if (object === null){
+        return object;
+    } else if (typeof object === 'object'){
+        if (Array.isArray(object)){
+            return copyArray(object);
+        } else {
+            return copyObject(object);
+        }
+    } else {
+        return object;
+    }
+}
+
+function copyObject(_obj){
+    var copyObj = {}
+    for (var key in _obj) {
+        if (_obj.hasOwnProperty(key)) {
+            copyObj[key] = copy(_obj[key])
+        }
+    }
+    return copyObj;
+}
+
+function copyArray(_obj){
+    var copyArray = []
+    for (var i = 0; i < _obj.length; i++){
+        copyArray[i] = copy(_obj[i])
+    }
+    return copyArray;
+}
+
+exports.init = function(filepath) {
+    dataFilePath = filepath;
+    load();
+}
+
+exports.put = function (key, value, callback) {
+    load();
+    data[key] = value;
+    save(callback);
+}
+
+exports.getAllSettings = function() {
+    load();
+    return copy(data);
+}
+
+exports.settingsReference = function() {
+    return data;
+}
+
+exports.saveAll = function(settings, callback) {
+    load();
+    data = settings;
+    save(callback)
+}
+
+exports.get = function (key) {
+    load();
+    var value = null;
+    if (key in data) {
+        value = copy(data[key]);
+    }
+    return value;
+}
+
+exports.unset = function (key, callback) {
+    load();
+    if (key in data) {
+        delete data[key];
+        save(callback);
+    }
+}
