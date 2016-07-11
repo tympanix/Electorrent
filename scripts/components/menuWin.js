3b7e6082adde8b9aa92a33171efc8078cdd147d0
Mathias Mortensen <tympanix@gmail.com>
2016-07-12 00:45:11 +0200

    macOS fixes and improvements

--- scripts/components/menuWin.js
+++ scripts/components/menuWin.js
@@ -1,4 +1,4 @@
-angular.module('torrentApp').factory("menu", ['electron', '$rootScope', function(electron, $rootScope) {
+angular.module('torrentApp').factory("menuWin", ['electron', '$rootScope', function(electron, $rootScope) {
     const template = [
         {
             label: 'Edit',
@@ -107,7 +107,7 @@ angular.module('torrentApp').factory("menu", ['electron', '$rootScope', function
             submenu: [
                 {
                     label: 'Learn More',
-                    click() { electron.shell.openExternal('http://electron.atom.io'); }
+                    click() { electron.shell.openExternal('https://github.com/Tympanix/Electorrent'); }
                 },
                 {
                     label: 'Check For Updates',
@@ -116,82 +116,6 @@ angular.module('torrentApp').factory("menu", ['electron', '$rootScope', function
             ]
         },
     ];
-
-    if (process.platform === 'darwin') {
-        const name = electron.app.getName();
-        template.unshift({
-            label: name,
-            submenu: [
-                {
-                    label: 'About ' + name,
-                    role: 'about'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Preferences...',
-                    click: function() {
-                        console.log("Preferences");
-                    }
-                },
-                {
-                    label: 'Services',
-                    role: 'services',
-                    submenu: []
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Hide ' + name,
-                    accelerator: 'Command+H',
-                    role: 'hide'
-                },
-                {
-                    label: 'Hide Others',
-                    accelerator: 'Command+Alt+H',
-                    role: 'hideothers'
-                },
-                {
-                    label: 'Show All',
-                    role: 'unhide'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Quit',
-                    accelerator: 'Command+Q',
-                    click() { electron.app.quit(); }
-                },
-            ]
-        });
-        // Window menu.
-        template[3].submenu = [
-            {
-                label: 'Close',
-                accelerator: 'CmdOrCtrl+W',
-                role: 'close'
-            },
-            {
-                label: 'Minimize',
-                accelerator: 'CmdOrCtrl+M',
-                role: 'minimize'
-            },
-            {
-                label: 'Zoom',
-                role: 'zoom'
-            },
-            {
-                type: 'separator'
-            },
-            {
-                label: 'Bring All to Front',
-                role: 'front'
-            }
-        ];
-    }
 
     return template;
 
