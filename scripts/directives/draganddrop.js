6ceb176c56a976ce5f178928784e33e91e661ea3
Mathias Mortensen <tympanix@gmail.com>
2016-09-01 21:22:46 +0200

    Drag and drop animation

--- scripts/directives/draganddrop.js
+++ scripts/directives/draganddrop.js
@@ -1,9 +1,44 @@
-angular.module("torrentApp").directive('dragAndDrop', ['electron', function(electron) {
+angular.module("torrentApp").directive('dragAndDrop', ['$rootScope', '$document', 'electron', function($rootScope, $document, electron) {
     return function(scope, element, attrs) {
+
+        var dragging = 0;
 
         document.ondragover = document.ondrop = function(event) {
             event.preventDefault()
         }
+
+        element.bind('click', function() {
+            dragging = 0;
+            $rootScope.$emit('show:draganddrop', false);
+        });
+
+        element.bind('dragenter', function(event, data) {
+            dragging++;
+
+            console.log("Dragenter", dragging);
+            $rootScope.$emit('show:draganddrop', true);
+
+            event.stopPropagation();
+            event.preventDefault();
+
+            return false;
+        })
+
+        element.bind('dragleave', function (event, data) {
+
+            dragging--;
+            console.log('Dragleave', dragging);
+
+            if (dragging === 0) {
+                $rootScope.$emit('show:draganddrop', false);
+            }
+
+            event.stopPropagation();
+            event.preventDefault();
+
+            return false;
+
+        })
 
         element.bind('drop', function(event, data) {
             var files = event.originalEvent.dataTransfer.files;
@@ -17,6 +52,8 @@ angular.module("torrentApp").directive('dragAndDrop', ['electron', function(elec
             electron.upload(paths);
 
             console.log("Paths", paths);
+
+            $rootScope.$emit('show:draganddrop', false);
 
             console.info("Drop!", event.originalEvent.dataTransfer.files);
         });
