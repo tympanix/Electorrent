25e2c04d33bc0ffc7c847f7c02c16373f7921c16
Mathias <tympanix@gmail.com>
2017-01-30 18:26:27 +0100

    Fixed do not show zero as a value for queue

--- scripts/filters/torrentfilters.js
+++ scripts/filters/torrentfilters.js
@@ -1,6 +1,6 @@
 angular.module("torrentApp").filter('torrentQueue', function() {
     return function(queue) {
-        if (Number.isInteger(queue) && queue >= 0) {
+        if (Number.isInteger(queue) && queue > 0) {
             return queue
         } else {
             return ''
