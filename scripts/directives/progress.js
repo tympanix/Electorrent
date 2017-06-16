47b8bfb35d007b2578cf62289257e6b02a8affa3
Mathias <tympanix@gmail.com>
2017-06-17 01:35:04 +0200

    Custom compile and link function for table rows

--- scripts/directives/progress.js
+++ scripts/directives/progress.js
@@ -1,34 +1,31 @@
 angular.module("torrentApp").directive('progress', function() {
     return {
         scope: {
-            torrent: '=',
+            torrent: '=progress',
         },
-        restrict: 'E',
-        template: `<div class="ui torrent progress" ng-class="class()" progress="torrent">
+        restrict: 'A',
+        template: `<div class="ui torrent progress" ng-class="class()">
             <label>{{label()}}</label>
             <div class="bar"></div>
         </div>`,
-        controller: controller,
         replace: true,
         link: link
     };
 
-    function controller($scope){
 
-        $scope.class = function(){
-            return $scope.torrent.statusColor();
+    function link(scope, element /*, attrs*/ ) {
+
+        scope.class = function(){
+            return scope.torrent.statusColor();
         }
 
-        $scope.label = function(){
-            var label = $scope.torrent.statusText();
-            if ($scope.torrent.isStatusDownloading()){
+        scope.label = function(){
+            var label = scope.torrent.statusText();
+            if (scope.torrent.isStatusDownloading()){
                 label += (" " + $scope.torrent.getPercentStr());
             }
             return label;
         }
-    }
-
-    function link(scope, element /*, attrs*/ ) {
 
         scope.$watch(function() {
             return scope.torrent.percent;
