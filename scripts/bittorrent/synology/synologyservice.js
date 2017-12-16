a62e75c22bbc582d452c4d4a020d1c8a626ce727
Pete <pafischer1992@gmail.com>
2017-12-16 19:36:24 +0100

    Timeout increased to 6 secs.
    Synology has its own timeout of 5 seconds before returning whether or not login has failed or not, thus causing Electorrent to display a timeout error instead of the proper "Login Failed" error.

--- scripts/bittorrent/synology/synologyservice.js
+++ scripts/bittorrent/synology/synologyservice.js
@@ -37,7 +37,7 @@ angular.module('torrentApp').service('synologyService', ["$http", "$q", "Torrent
                 method: method,
                 query: query
             }),
-            timeout: 5000
+            timeout: 6000
         }
     }
 
