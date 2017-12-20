23dbb5720c5831b95fddac5ba9d75eaeb4963f66
Mathias <tympanix@gmail.com>
2017-12-20 16:49:43 +0100

    Restructure application source into src folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,34 @@
 'use strict';
 
+const fs = require('fs');
 const gulp = require('gulp');
-const electron = require('electron-connect').server.create();
+const {server} = require('electron-connect')
 const useref = require('gulp-useref');
 const clean = require('gulp-clean');
-const runSequence = require('run-sequence');
+const rename = require('gulp-rename');
+const util = require('gulp-util');
 const less = require('gulp-less');
 const path = require('path');
+const merge = require('merge-stream');
 
+const PROD = process.env.NODE_ENV === 'production' || !!util.env.production
 const OUT = "./app";
 const CLEAN = [`!${OUT}/package.json`, `!${OUT}/node_modules`, `${OUT}/*`];
+const semantic = './bower_components/semantic/src'
 
 gulp.task('serve', function () {
+    let electron = server.create({
+      path: 'app'
+    });
 
     // Start browser process
     electron.start('--debug');
 
     // Restart browser process
-    gulp.watch(['app.js', 'lib/*.js'], electron.restart);
+    gulp.watch(['src/app.js', 'src/lib/*.js'], electron.restart);
 
     // Reload renderer process
-    gulp.watch(['main.js', 'index.html', 'css/**/*', 'scripts/**/*', 'views/**/*'], electron.reload);
+    gulp.watch(['src/**/*.{html,cs,js}'], electron.reload);
 });
 
 gulp.task('default', ['serve']);
@@ -31,18 +39,22 @@ gulp.task('build:clean', function() {
 });
 
 gulp.task('build:concat', function() {
-    return gulp.src('./*.html')
-    .pipe(useref())
+    return gulp.src('src/*.html')
+    .pipe(useref({
+      dev: function(content) {
+        return PROD ? '' : content
+      }
+    }))
     .pipe(gulp.dest(OUT))
 });
 
 gulp.task('build:app', function() {
-    return gulp.src(['./app.js'])
+    return gulp.src(['src/app.js'])
     .pipe(gulp.dest(OUT));
 });
 
 gulp.task('build:static', function() {
-    return gulp.src(['./views/**/*', './lib/**/*', './css/fonts/**/*', './img/**/*', './build/**/*'], { base: './'})
+    return gulp.src(['src/views/**/*', 'src/lib/**/*', 'src/css/fonts/**/*', 'src/img/**/*', 'build/**/*'], { base: 'src'})
     .pipe(gulp.dest(OUT))
 })
 
@@ -51,41 +63,64 @@ gulp.task('build:assets' , function () {
     .pipe(gulp.dest(OUT + '/css/themes/default/assets'))
 });
 
-const semantic = './bower_components/semantic/src'
-
 gulp.task('src:semantic', function() {
   return gulp.src([
     path.join(semantic, '**'),
     '!'+path.join(semantic, 'themes/**'),
     '!'+path.join(semantic, 'themes/')
-  ]).pipe(gulp.dest('css/semantic'))
+  ]).pipe(gulp.dest('src/css/semantic'))
 })
 
 gulp.task('themes:semantic', function() {
   let themes = ['default']
   return gulp.src(themes.map(t => path.join(semantic, 'themes', t, '/**')),
    {base: path.join(semantic, 'themes')})
-    .pipe(gulp.dest('css/themes'))
+    .pipe(gulp.dest('src/css/themes'))
 })
 
 gulp.task('semantic', ['src:semantic', 'themes:semantic'])
 
 gulp.task('build:less', ['semantic'], function() {
-  return gulp.src('css/semantic/semantic.less')
+  return gulp.src('src/css/semantic/semantic.less')
     .pipe(less({
       paths: [semantic],
       globalVars: {
-        "@renderTheme": 'slate'
+        "@renderTheme": 'dark'
       }
     }))
     .pipe(gulp.dest(`${OUT}/css`))
 })
 
+gulp.task('themes:all', ['semantic'], function() {
+  let dir = 'src/css/themes'
+  let themes = fs.readdirSync(dir)
+    .filter(function(file) {
+      let stat = fs.statSync(path.join(dir, file))
+      return stat.isDirectory() && file !== 'default'
+    })
+
+  let tasks = themes.map(function(theme) {
+    return gulp.src('src/css/semantic/semantic.less')
+      .pipe(less({
+        paths: [semantic],
+        globalVars: {
+          "@renderTheme": theme
+        }
+      }))
+      .pipe(rename({
+        basename: theme
+      }))
+      .pipe(gulp.dest(path.join(OUT, 'css', 'themes')))
+  })
+
+  return merge(tasks)
+})
+
 gulp.task('styles', function() {
-  return gulp.src('css/styles.less')
+  return gulp.src('src/css/styles.less')
     .pipe(less({
       globalVars: {
-        "@renderTheme": 'slate'
+        "@renderTheme": 'dark'
       }
     }))
     .pipe(gulp.dest(`${OUT}/css`))
@@ -93,8 +128,6 @@ gulp.task('styles', function() {
 
 gulp.task('theme', ['build:less', 'styles'])
 
-gulp.task('build', function() {
-    runSequence('build:clean', ['build:concat', 'build:app', 'build:assets', 'build:static', 'build:less']);
-});
+gulp.task('build', ['build:concat', 'build:app', 'build:assets', 'build:static', 'build:less']);
 
 gulp.task('install', ['semantic'])
