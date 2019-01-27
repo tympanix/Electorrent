761d278c956f458066690ad51c4e5c5c8ffbfeea
tympanix <tympanix@gmail.com>
2019-01-28 00:29:12 +0100

    fix: ctrl+A on windows, resolves #140

--- src/scripts/components/menuWin.js
+++ src/scripts/components/menuWin.js
@@ -1,5 +1,13 @@
 angular.module('torrentApp').factory("menuWin", ['electron', '$rootScope', '$bittorrent', 'notificationService', 'configService', function(electron, $rootScope, $bittorrent, $notify, config) {
 
+    Mousetrap.bind('ctrl+a', function() {
+        if (document.activeElement.nodeName === 'INPUT') {
+            electron.remote.getCurrentWebContents().selectAll()
+        } else {
+            $rootScope.$broadcast('menu:selectall')
+        }
+    })
+
     const template = [
         {
             label: 'File',
