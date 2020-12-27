b28b1d9d8986559176141c86e1b4b54279ae1674
tympanix <tympanix@gmail.com>
2020-12-27 18:25:12 +0100

    refactor: deluge es6 module

--- src/scripts/bittorrent/deluge/delugeservice.ts
+++ src/scripts/bittorrent/deluge/delugeservice.ts
@@ -1,183 +1,121 @@
+import {ContextActionList, TorrentActionList, TorrentClient, TorrentUpdates} from "../torrentclient";
+import {DelugeTorrent} from "./torrentd";
 
-export let delugeService = ["TorrentD", "$q", "$remote", "notificationService", function(TorrentD, $q, $remote, $notify) {
+type CallbackFunc = (err: any, val: any) => void
 
-    const Deluge = require('@electorrent/node-deluge')
-    const worker = new Worker('scripts/workers/deluge.js')
+function defer<T>(fn: (f: CallbackFunc) => void): Promise<T> {
+  return new Promise((reject, resolve) => {
+    fn((err, val) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(val)
+      } 
+    })
+  })
+}
 
-    /*
-     * Please rename all occurences of __serviceName__ (including underscores) with the name of your service.
-     * Best practise is naming your service starting with the client name in lower case followed by 'Service'
-     * (remember capital 'S' e.g qbittorrentService for qBittorrent, utorrentService for µTorrent ect.).
-     * The real name of your client for display purposes can be changes in the field 'this.name' below.
-     */
-    this.name = 'Deluge';
+const Deluge = require('@electorrent/node-deluge')
 
-    /*
-     * Global reference to the deluge remote web worker instance
-     */
-    let deluge = null
+export class DelugeClient extends TorrentClient<DelugeTorrent> {
+
+    name = 'Deluge';
+
+    private deluge = null
 
 
-    /**
-     * Connect to the server upon initial startup, changing connection settings ect. The function
-     * should return a promise that the connection was successfull. A standard http timeout of 5 seconds
-     * must be implemented. When successfull the service should save login details for later use. Check out
-     * the helper function on the `server` object. Especially the `url()` function is useful.
-     * @param {server} server
-     * @return {promise} connection
-     */
-    this.connect = function(server) {
-        deluge = new $remote(Deluge.prototype, worker)
-
-        let ca = server.getCertificate()
-
-        return deluge.instantiate({
+    async connect(server): Promise<void> {
+        this.deluge = new Deluge({
             host: server.url(),
             port: server.port,
             path: server.cleanPath(),
             pass: server.password,
-            ca: ca,
-        }).then(function() {
-            return deluge.login()
-        }).then(function() {
-            // Connect to server #0 by default
-            return deluge.connect(0)
+            ca: server.getCertificate(),
         })
+
+        await defer(done => this.deluge.login(done))
+        // Connect to server #0 by default
+        return await this.deluge.connect(0)
     }
 
-    /**
-     * Return any new information about torrents to be rendered in the GUI. Should return a
-     * promise with the required information to be updated. Will be executed by controllers
-     * very frequently. You can find a template of the data to be returned in the function.
-     * Whenever boolean fullupdate is true this function should return a full list of all
-     * the information from the client.
-     * Returned information will have the following format:
-     *      labels {array}: array of string of each label
-     *      all {array}: array of objects inherited from 'AbstractTorrent' that are not currently known.
-     *              This means they have just been added or never seen before since the last startup.
-     *      changed {array}: array of objects inherited from 'AbstractTorrent' that have already been send before.
-     *              This means they may contain partial information in which case they ar merged with any present infomation.
-     *      deleted {array}: array of string containg the hashes of which torrents to be removed from the list in the GUI.
-     * @param {boolean} fullupdate
-     * @return {promise} data
-     */
-    this.torrents = function() {
-        return deluge.getTorrents().then((data) => {
-            var torrents = {
-                labels: [],
-                all: [],
-                changed: [],
-                deleted: [],
-                dirty: true,
-            };
+    async torrents(): Promise<TorrentUpdates> {
+        var torrents = {
+            labels: [],
+            all: [],
+            changed: [],
+            deleted: [],
+            dirty: true,
+        };
 
-            for (const hash of Object.keys(data.torrents || {})) {
-                torrents.all.push(new TorrentD(hash, data.torrents[hash]))
-            }
+        let data: Record<string, any> = await defer(done => this.deluge.getTorrents(done))
 
-            return torrents
-        })
+        for (const hash of Object.keys(data.torrents || {})) {
+            torrents.all.push(new DelugeTorrent(hash, data.torrents[hash]))
+        }
+
+        return torrents
     }
 
-    /**
-     * Returns the default path for the service. Should start with a slash.
-     @return {string} the default path
-     */
-    this.defaultPath = function() {
+    defaultPath(): string {
       return "/"
     }
 
-    /**
-     * Add a torrent to the client by sending a magnet link to the API. Should return
-     * a promise that the torrent has been added successfully to the client.
-     * @param {string} magnetURL
-     * @return {promise} isAdded
-     */
-    this.addTorrentUrl = function(magnet) {
-        return deluge.addTorrentURL(magnet, {})
+    addTorrentUrl(magnet: string): Promise<void> {
+        return defer(done => this.deluge.addTorrentURL(magnet, {}, done))
     }
 
-    /**
-     * Add a torrent file with the .torrent extension to the client through the API. Should
-     * return a promise that the torrent was added sucessfully. File data is given as an nodejs buffer
-     * more information here: https://nodejs.org/api/buffer.html. You may use
-     * the existing implementation as a helping hand
-     * @param {blob} filedata
-     * @param {string} filename
-     * @return {promise} isAdded
-     */
-    this.uploadTorrent = function(buffer, filename) {
-        return deluge.addTorrent(buffer, {})
+    uploadTorrent(buffer: Blob, filename: string): Promise<void> {
+        return defer(done => this.deluge.addTorrent(buffer, {}, done))
     }
 
-    /**
-     * Example action function. You will have to implement several of these to support the various
-     * actions in your bittorrent client. Each action is supplied an array of the torrents on which
-     * the action should be applied. The torrent object is the same type of object which you implemented
-     * alongside your service (e.g. TorrentU for µTorrent, TorrentQ for qBittorrent ect...)
-     * @param {array} torrents
-     * @return {promise} actionIsDone
-     */
-    this.resume = function(torrents) {
-        return deluge.resume(torrents.map(t => t.hash))
+    resume(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.resume(torrents.map(t => t.hash), done))
     }
 
-    this.pause = function(torrents) {
-        return deluge.pause(torrents.map(t => t.hash))
+    pause(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.pause(torrents.map(t => t.hash), done))
     }
 
-    this.verify = function(torrents) {
-        return deluge.verify(torrents.map(t => t.hash))
+    verify(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.verify(torrents.map(t => t.hash), done))
     }
 
-    this.remove = function(torrents) {
-        return deluge.remove(torrents.map(t => t.hash))
+    remove(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.remove(torrents.map(t => t.hash), done))
     }
 
-    this.removeAndDelete = function(torrents) {
-        return deluge.removeAndDelete(torrents.map(t => t.hash))
+    removeAndDelete(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.removeAndDelete(torrents.map(t => t.hash), done))
     }
 
-    this.queueUp = function(torrents) {
-        return deluge.queueUp(torrents.map(t => t.hash))
+    queueUp(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.queueUp(torrents.map(t => t.hash), done))
     }
 
-    this.queueDown = function(torrents) {
-        return deluge.queueDown(torrents.map(t => t.hash))
+    queueDown(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.queueDown(torrents.map(t => t.hash), done))
     }
 
-    this.queueTop = function(torrents) {
-        return deluge.queueTop(torrents.map(t => t.hash))
+    queueTop(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.queueTop(torrents.map(t => t.hash), done))
     }
 
-    this.queueBottom = function(torrents) {
-        return deluge.queueBottom(torrents.map(t => t.hash))
+    queueBottom(torrents: DelugeTorrent[]): Promise<void> {
+        return defer(done => this.deluge.queueBottom(torrents.map(t => t.hash), done))
     }
 
     /**
      * Whether the client supports sorting by trackers or not
      */
-    this.enableTrackerFilter = false
+    enableTrackerFilter = false
 
     /**
      * Provides the option to include extra columns for displaying data. This may concern columns
      * which are specific to this client. The extra columns will be merged with the default columns.
      */
-    this.extraColumns = []
+    extraColumns = []
 
-
-    /**
-     * Represents the buttons and GUI elements to be displayed in the top navigation bar of the windows.
-     * You may customize the GUI to your liking or to better accommodate the specific bittorrent client.
-     * Every action must have a click function that corresponds to an action like the one showed above.
-     * An object in the array should consist of the following information:
-     *      label [string]: Name of the button/element
-     *      type [string]: Can be 'button' or 'dropdown' or 'labels'
-     *      color [string]: Can be 'red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'
-     *      click [function]: The function to be executed when the when the button/element is pressed
-     *      icon [string]: The icon of the button. See here: http://semantic-ui.com/elements/icon.html
-     */
-    this.actionHeader = [
+    actionHeader: TorrentActionList<DelugeTorrent> = [
         {
             label: 'Start',
             type: 'button',
@@ -196,17 +134,7 @@ export let delugeService = ["TorrentD", "$q", "$remote", "notificationService", 
         },
     ]
 
-    /**
-     * Represents the actions available in the context menu. Can be customized to your liking or
-     * to better accommodate your bittorrent client. Every action must have a click function implemented.
-     * Each element has an:
-     *      label [string]:     The name of the action
-     *      click [function]:   The function to be executed when clicked
-     *      icon [string]:      The icon of the action. See here: http://semantic-ui.com/elements/icon.html
-     *      check [function]:   Displays a checkbox instead of an icon. The function is a predicate which
-     *                          has to hold for all selected torrents, for the checkbox to be checked.
-     */
-    this.contextMenu = [
+    contextMenu: ContextActionList<DelugeTorrent> = [
         {
             label: 'Verify',
             click: this.verify,
@@ -245,4 +173,5 @@ export let delugeService = ["TorrentD", "$q", "$remote", "notificationService", 
         },
     ];
 
-}];
+}
+
--- src/scripts/bittorrent/deluge/torrentd.ts
+++ src/scripts/bittorrent/deluge/torrentd.ts
@@ -1,21 +1,11 @@
+import {Torrent} from "../abstracttorrent";
 
-export let TorrentD = ['AbstractTorrent', function(AbstractTorrent) {
+export class DelugeTorrent extends Torrent {
 
-    /**
-     * Constructor, with class name.
-     * Replace all occurences of TorrentD (including underscores) with
-     * the name of your torrent class. Best practise is naming the class 'Torrent'
-     * and appending a single letter desribing the client to which it belongs.
-     * (e.g. TorrentQ for qBittorrent, TorrentU for µTorrent... and so on)
-     */
-    function TorrentD(hash, data) {
-        /*
-         * Please modify the constructor function parameters to
-         * your liking for the best implementation. If data is obtained as an array from
-         * the API one could list each function parameter in the same order as the array
-         */
+    public state: string
 
-        AbstractTorrent.call(this, {
+    constructor(hash: string, data: Record<string, any>) {
+        super({
             hash: hash, /* Hash (string): unique identifier for the torrent */
             name: data.name, /* Name (string): the name of the torrent */
             size: data.total_wanted, /* Size (integer): size of the file to be downloaded in bytes */
@@ -43,98 +33,34 @@ export let TorrentD = ['AbstractTorrent', function(AbstractTorrent) {
          * may be added as extra fields. This can be done in the manner below
          */
         this.state = data.state
-
     }
 
-    /*
-     * Inherit by prototypal inheritance. Leave this line as is (only rename class name).
-     * Do NOT implement any prototypal features above this line!
-     */
-    TorrentD.prototype = Object.create(AbstractTorrent.prototype);
-
-
-    /**
-     * Returns whether this torrent is in an error state. Torrents in this group shows
-     * up in the 'Error' tab in the GUI
-     * @return {boolean} isStatusError
-     */
-    TorrentD.prototype.isStatusError = function() {
+    isStatusError(): boolean {
         return this.state === "Error"
     };
 
-    /**
-     * Returns whether this torrent is stopped. Torrents in this group shows up in
-     * the 'Stopped' tab to the left in the GUI
-     * @return {boolean} isStatusStopped
-     */
-    TorrentD.prototype.isStatusStopped = function() {
+    isStatusStopped(): boolean {
         return this.state === "Paused"
     };
 
-    /**
-     * Returns whether this torrent is in queue for downloading. Torrents in this group shows up
-     * in the same tab as 'Downloading' in the GUI
-     * @return {boolean} isStatusQueue
-     */
-    TorrentD.prototype.isStatusQueued = function() {
+    isStatusQueued(): boolean {
         return this.state === "Queued"
     };
 
-    /**
-     * Returns whether this torrent is completed. Usually this can be done by
-     * checking whether this.percent === 1000 which means 100%. Torrents in this group
-     * shows up the 'Finished' tab in the GUI
-     * @return {boolean} isStatusCompleted
-     */
-    TorrentD.prototype.isStatusCompleted = function() {
+    isStatusCompleted(): boolean {
         return this.percent === 1000
     };
 
-    /**
-     * Returns whether this torrent is downloading. Torrents in this group
-     * shows up the 'Downloading' tab in the GUI
-     * @return {boolean} isStatusDownloading
-     */
-    TorrentD.prototype.isStatusDownloading = function() {
+    isStatusDownloading(): boolean {
         return this.state === "Downloading"
     };
 
-    /**
-     * Returns whether this torrent is seeding. Torrents in this group
-     * shows up the 'Seeding' tab in the GUI
-     * @return {boolean} isStatusDownloading
-     */
-    TorrentD.prototype.isStatusSeeding = function() {
+    isStatusSeeding() {
         return this.state === "Seeding"
     };
 
-    /**
-     * Returns whether this torrent is paused. Torrents in this group
-     * shows up in the same tab as the 'Downloading' tab in the GUI.
-     * @return {boolean} isStatusDownloading
-     */
-    TorrentD.prototype.isStatusPaused = function() {
+    isStatusPaused() {
         return false
     };
 
-    /**
-     * Optionally returns the color for the progress bar used as a class in CSS.
-     * Colors are decided by default using the status functions above. Only implement
-     * this when having color issues.
-     * @return {string} color
-     */
-    /*TorrentD.prototype.statusColor = function () {};*/
-
-    /**
-     * Optionally returns the status of the torrent. The status is by default
-     * computed using the.statusMessage. Only implement this when having trouble with
-     * incorrect status messages in the GUI
-     * @return {string} status
-     */
-    /*TorrentD.prototype.statusText = function () { };*/
-
-    /**
-     * Return the constructor function (only change the class name)
-     */
-    return TorrentD;
-}];
+}
