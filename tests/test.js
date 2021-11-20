ac7895d7893dea232edfb20c635118b7db8425e0
tympanix <tympanix@gmail.com>
2021-11-20 23:17:26 +0100

    refactor: use docker-compose for test fixtures

--- tests/test.js
+++ tests/test.js
@@ -1,70 +1,25 @@
 const path = require("path");
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
-
-const testlib = require("./testlib");
+const compose = require("docker-compose")
 
 global.before(function () {
   chai.should();
   chai.use(chaiAsPromised);
 });
 
-testlib.testclient({
-  client: "transmission",
-  dockerContainer: "linuxserver/transmission",
-  dockerEnv: {
-    USER: 'username',
-    PASS: 'password'
-  },
-  host: "127.0.0.1",
-  port: 9091,
-  containerPort: 9091,
-  username: "username",
-  password: "password",
-  acceptHttpStatus: 401,
-  skipTests: ["labels"]
-});
+describe("test clients", async function() {
 
-testlib.testclient({
-  client: "deluge",
-  dockerContainer: "spritsail/deluge:1.3.15",
-  host: "127.0.0.1",
-  port: 8112,
-  containerPort: 8112,
-  username: "admin",
-  password: "deluge",
-  stopLabel: "Paused",
-  skipTests: ["labels"],
-});
+  // Allow docker-compose up/down to complete
+  this.timeout(25 * 1000)
 
-testlib.testclient({
-  client: "utorrent",
-  dockerContainer: "ekho/utorrent",
-  host: "127.0.0.1",
-  port: 8080,
-  containerPort: 8080,
-  username: "admin",
-  password: "",
-  acceptHttpStatus: 400,
-});
+  before(async function() {
+      await compose.upAll({ cwd: path.join(__dirname), log: true })
+  })
 
-testlib.testclient({
-  client: "rtorrent",
-  dockerContainer: "linuxserver/rutorrent",
-  host: "127.0.0.1",
-  port: 80,
-  containerPort: 80,
-  acceptHttpStatus: 200,
-  username: "admin",
-  password: "admin",
-});
+  require("./suites")()
 
-testlib.testclient({
-  client: "qbittorrent",
-  dockerContainer: "linuxserver/qbittorrent",
-  host: "127.0.0.1",
-  containerPort: 8080,
-  port: 8080,
-  username: "admin",
-  password: "adminadmin",
-});
+  after(async function() {
+      await compose.down({ cwd: path.join(__dirname), log: true })
+  })
+})
