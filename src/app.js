778fe7a60d144e82ec4b2b396ae5de6055e053ed
tympanix <tympanix@gmail.com>
2022-09-23 22:49:59 +0200

    chore: remove unnecessary console logs

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ const path = require('path');
 const is = require('electron-is');
 require('@electron/remote/main').initialize();
 
-console.log("Starting app")
-
 // Handle Squirrel startup parameters
 if (require('./lib/startup')) return
 
