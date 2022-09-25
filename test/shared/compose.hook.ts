cce92a36c3124b72151a49d804f7335844b621f9
tympanix <tympanix@gmail.com>
2022-09-25 15:32:44 +0200

    test: advanced upload options

--- test/shared/compose.hook.ts
+++ test/shared/compose.hook.ts
@@ -4,7 +4,8 @@ import path from "path"
 /**
  * Mocha hooks to start up and shut down a docker-compose service using the "before" and
  * "after" hooks. This means the services described in your docker-compose.yml file will
- * be running for the entire current mocha context
+ * be running for the entire current mocha context. Before the docker-compose services
+ * are started all existing containers are removed to ensure integrity of the test fixture
  * @param composeDir the directory containing your docker-compose.yml file
  * @param extraOpts additonal options to the docker-compose invocation
  */
@@ -21,6 +22,7 @@ export function dockerComposeHooks(composeDir: string | string[], extraOpts?: ID
 
   before(async function() {
     this.timeout(60 * 1000)
+    await compose.rm({ ...composeOpts, ...extraOpts})
     await compose.upAll({ ...composeOpts, ...extraOpts, commandOptions: ['--build'] })
   })
 
