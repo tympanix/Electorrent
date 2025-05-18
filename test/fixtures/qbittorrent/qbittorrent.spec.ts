aedb5e9a277ad1f23f3b112b5a7cc01cf925ae87
tympanix <tympanix@gmail.com>
2025-05-18 19:32:25 +0200

    fix: lock qbittorrent version tags for e2e test

--- test/fixtures/qbittorrent/qbittorrent.spec.ts
+++ test/fixtures/qbittorrent/qbittorrent.spec.ts
@@ -5,7 +5,7 @@ import { QBittorrentClient } from "../../../src/scripts/bittorrent"
 createTestSuite({
   client: new QBittorrentClient(),
   fixture: "fixtures/qbittorrent",
-  version: "5",
+  version: "latest",
   port: 8080,
   username: "admin",
   password: "adminadmin",
@@ -15,7 +15,7 @@ createTestSuite({
 createTestSuite({
   client: new QBittorrentClient(),
   fixture: "fixtures/qbittorrent",
-  version: "4",
+  version: "5.1.0",
   port: 8080,
   username: "admin",
   password: "adminadmin",
@@ -25,7 +25,17 @@ createTestSuite({
 createTestSuite({
   client: new QBittorrentClient(),
   fixture: "fixtures/qbittorrent",
-  version: "3",
+  version: "4.6.7",
+  port: 8080,
+  username: "admin",
+  password: "adminadmin",
+  unsupportedFeatures: [],
+});
+
+createTestSuite({
+  client: new QBittorrentClient(),
+  fixture: "fixtures/qbittorrent",
+  version: "3.3.15",
   port: 8080,
   username: "admin",
   password: "adminadmin",
