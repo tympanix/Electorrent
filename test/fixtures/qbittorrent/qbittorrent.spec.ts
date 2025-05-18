7647ce25201f190f9adaf5f218b2956b9b934d9a
tympanix <tympanix@gmail.com>
2025-05-18 17:02:27 +0200

    feature: e2e tests for qbittorrent v3, v4 and v5

--- test/fixtures/qbittorrent/qbittorrent.spec.ts
+++ test/fixtures/qbittorrent/qbittorrent.spec.ts
@@ -1,11 +1,31 @@
 import { createTestSuite } from "../../testlib";
-import { FeatureSet } from "../../testutil";
 
 import { QBittorrentClient } from "../../../src/scripts/bittorrent"
 
 createTestSuite({
   client: new QBittorrentClient(),
   fixture: "fixtures/qbittorrent",
+  version: "5",
+  port: 8080,
+  username: "admin",
+  password: "adminadmin",
+  unsupportedFeatures: [],
+});
+
+createTestSuite({
+  client: new QBittorrentClient(),
+  fixture: "fixtures/qbittorrent",
+  version: "4",
+  port: 8080,
+  username: "admin",
+  password: "adminadmin",
+  unsupportedFeatures: [],
+});
+
+createTestSuite({
+  client: new QBittorrentClient(),
+  fixture: "fixtures/qbittorrent",
+  version: "3",
   port: 8080,
   username: "admin",
   password: "adminadmin",
