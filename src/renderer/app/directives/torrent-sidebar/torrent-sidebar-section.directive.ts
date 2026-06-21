638887b6cef54b5f967289e61d9aafd0c36b8244
tympanix <tympanix@gmail.com>
2026-06-21 17:37:15 +0200

    refactor: torrent-sidebar directive

---
+++ src/renderer/app/directives/torrent-sidebar/torrent-sidebar-section.directive.ts
@@ -0,0 +1,45 @@
+import { IDirective, IDirectiveFactory } from "angular";
+import html from "./torrent-sidebar-section.template.html";
+
+export class TorrentSidebarSectionController {
+    items: string[] = [];
+    active?: string;
+    title = "";
+    clearRole = "";
+    emptyText = "";
+    itemAttribute = "";
+    onSelect?: (locals: { item?: string }) => void;
+
+    isActive(item: string) {
+        return this.active === item;
+    }
+
+    select(item: string) {
+        this.onSelect?.({ item });
+    }
+
+    clear() {
+        this.onSelect?.({ item: undefined });
+    }
+}
+
+export class TorrentSidebarSectionDirective implements IDirective {
+    restrict = "E";
+    scope = {
+        items: "=",
+        active: "=",
+        title: "@",
+        clearRole: "@",
+        emptyText: "@",
+        itemAttribute: "@",
+        onSelect: "&",
+    };
+    bindToController = true;
+    controller = TorrentSidebarSectionController;
+    controllerAs = "ctl";
+    template = html;
+
+    static getInstance(): IDirectiveFactory {
+        return () => new TorrentSidebarSectionDirective();
+    }
+}
